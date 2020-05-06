c4dc59c6a33f2575a8e2bf0ef66cafc711292412
Jen Kagan <jen@netlify.com>
2020-05-06 14:13:00 -0700

    render struct

--- lessons-code-complete/core-concepts/3-using-environment-variables/functions/parse.go
+++ lessons-code-complete/core-concepts/3-using-environment-variables/functions/parse.go
@@ -13,22 +13,30 @@ import (
 type GoogleAppCreds struct {
 	Type			string	`json:"type"`
 	ProjectID		string	`json:"project_id"`
-	PrivateKeyID	string	`json:"private_key_id"`
+	PrivateKeyID		string	`json:"private_key_id"`
 	PrivateKey		string	`json:"private_key"`
 	ClientID		string	`json:"client_id"`
 	AuthURI			string	`json:"auth_uri"`
 	TokenURI		string	`json:"token_uri"`
-	AuthProvider	string	`json:"auth_provider_x509_cert_url"`
+	AuthProvider		string	`json:"auth_provider_x509_cert_url"`
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var gar GoogleAppCreds
+	
+	// Convert string to JSON and log a few values
 	json.Unmarshal([]byte(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")), &gar)
 	fmt.Println("projectID: ", gar.ProjectID)
 	fmt.Println("tokenURI: ", gar.TokenURI)
+	
+	// Convert JSON back to string to return as HTTP body
+	b, err := json.Marshal(gar)
+	if err != nil {
+        panic(err)
+	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Success",
+		Body:       string(b),
 	}, nil
 }
 
